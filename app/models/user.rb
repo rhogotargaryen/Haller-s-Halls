e2b27da32cd6a141f09f1fafe626329b602b9953
rhogotargaryen <dhaller.personal@gmail.com>
2018-11-30 23:55:52 -0800

    ok now its fully functioning

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
     validates :name, presence: true
 
     def self.find_or_create_by_oauth(auth)
-        where(email: auth.extra.raw_info.email).first_or_create do |user|
-          user.name = auth.extra.raw_info.name
-          user.name = "#{auth.extra.raw_info.name}#{user.name.length}"
+        where(email: auth.info.email).first_or_create do |user|
+          user.img_url = auth.info.image
+          user.name = auth.info.name
+          user.email = auth.info.email
           user.password = SecureRandom.hex
         end
     end
